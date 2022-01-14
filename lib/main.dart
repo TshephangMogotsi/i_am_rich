4112ee5c84817fb1f7e15b8ba71f6982bdd66797
TshephangMogotsi <zinjie7@gmail.com>
2022-01-14 16:12:24 +0200

    added network image

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,16 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
+        appBar: AppBar( 
           title: const Text("I Am Rich"),
           backgroundColor: Colors.blueGrey[900],
         ),
         backgroundColor: Colors.blueGrey[400],
+        body: const Center(
+          child: Image(
+            image: NetworkImage("https://images.unsplash.com/photo-1641935416268-98de86ac9c3d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=722&q=80")
+            ),
+        ),
       ),
     ),
   );
